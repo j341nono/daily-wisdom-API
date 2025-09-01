8bbcaad713e4cc14b29d01a5763e56015eded860
nonomura <kanakana.nono1224@gmail.com>
2025-09-01 22:41:58 +0900

    feat: Implement logic to add new rows to the quotes_table table

--- src/database/models.py
+++ src/database/models.py
@@ -4,10 +4,12 @@ SAVE_PATH: str = "data/data.json"
 
 DB_NAME = "src/database/data.db"
 
-CREATE_TABLE_TEXT = "CREATE TABLE IF NOT EXISTS quotes (" \
+CREATE_TABLE_TEXT = "CREATE TABLE IF NOT EXISTS quotes_table (" \
 "id INTEGER PRIMARY KEY AUTOINCREMENT," \
 "philosopher STRING," \
 "quotes STRING)"
+
+INSERT_TABLE_TEXT = "INSERT INTO quotes_table (philosopher, quotes) VALUES (?, ?)"
 
 
 def create_table():
@@ -17,5 +19,21 @@ def create_table():
     conn.commit()
     conn.close()
 
+
+def insert_qoute(philosopher: str, quotes: str):
+    conn = sqlite3.connect(DB_NAME)
+    cur = conn.cursor()
+    cur.execute(INSERT_TABLE_TEXT, (philosopher, quotes))
+    conn.commit()
+    conn.close()
+
+
+def debug_insert_table():
+    philosopher = "john"
+    quotes = "you can do it."
+    insert_qoute(philosopher=philosopher, quotes=quotes)
+
+
 if __name__ == "__main__":
     create_table()
+    debug_insert_table()
