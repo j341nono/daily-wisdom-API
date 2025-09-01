9f7150465bc9ca147d83378e8ebd0055c39a6536
nonomura <kanakana.nono1224@gmail.com>
2025-09-01 22:28:32 +0900

    fix: Prevent errors if the table already exists

--- src/database/models.py
+++ src/database/models.py
@@ -4,18 +4,18 @@ SAVE_PATH: str = "data/data.json"
 
 DB_NAME = "src/database/data.db"
 
-CREATE_TABLE_TEXT = "CREATE TABLE quotes (id INTEGER PRIMARY KEY AUTOINCREMENT," \
+CREATE_TABLE_TEXT = "CREATE TABLE IF NOT EXISTS quotes (" \
+"id INTEGER PRIMARY KEY AUTOINCREMENT," \
 "philosopher STRING," \
 "quotes STRING)"
 
 
-def main():
+def create_table():
     conn = sqlite3.connect(DB_NAME)
     cur = conn.cursor()
     cur.execute(CREATE_TABLE_TEXT)
-
     conn.commit()
     conn.close()
 
 if __name__ == "__main__":
-    main()
+    create_table()
