af8922bfcae480a079ed565c062a8b34f059a404
nonomura <kanakana.nono1224@gmail.com>
2025-09-02 01:26:56 +0900

    feat: Create a POST /items endpoint

--- src/api/api.py
+++ src/api/api.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 from src.database.models import (
     get_random_qoute,
+    insert_qoute,
 )
 
 
+class Item(BaseModel):
+    philosopher: str
+    quote: str
+
+
 app = FastAPI()
+
 
 @app.get("/")
 async def root():
@@ -19,3 +27,16 @@ async def _get_random():
     philosopher, quote = get_random_qoute()
     return {"philosopher": philosopher, "quote": quote}
 
+@app.post("/quotes")
+async def _post_quote(item: Item):
+    try:
+        insert_qoute(item.philosopher, item.quote)
+        success = True
+    except:
+        success = False
+    if success:
+        return {"success": success, "message": "Quote added successfully."}
+    else:
+        return {"success": success, "message": "Both 'philosopher' and 'quote' are required."}
+
+    
